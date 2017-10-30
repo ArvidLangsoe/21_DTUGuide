a4332645ec59dd77e1156bd4d43322b71785dd37
Jeppe <s093905@student.dtu.dk>
2017-10-30 21:35:47 +0100

    Added zoom/pan functionality

--- app/src/main/java/com/arvid/dtuguide/MainActivity.java
+++ app/src/main/java/com/arvid/dtuguide/MainActivity.java
@@ -3,11 +3,17 @@ package com.arvid.dtuguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alexvasilkov.gestures.views.interfaces.GestureView;
+
 public class MainActivity extends AppCompatActivity {
+    private GestureView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = (GestureView) findViewById(R.id.map);
+        map.getController().getSettings().setDoubleTapEnabled(false); // Falls back to max zoom level
     }
 }
