adc9ea19f29fac8fae8d600ad3c0c9531e027904
Jeppe <s093905@student.dtu.dk>
2017-11-23 13:23:57 +0100

    Added custom toolbar and side-navigation (Navigation Drawer)

--- app/src/main/java/com/arvid/dtuguide/MainActivity.java
+++ app/src/main/java/com/arvid/dtuguide/MainActivity.java
@@ -3,7 +3,10 @@ package com.arvid.dtuguide;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
+import com.alexvasilkov.gestures.views.GestureFrameLayout;
 import com.alexvasilkov.gestures.views.interfaces.GestureView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //map = (GestureView) findViewById(R.id.map);
-        //map.getController().getSettings().setDoubleTapEnabled(false); // Falls back to max zoom level
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        map = (GestureFrameLayout) findViewById(R.id.mapGFL);
+        map.getController().getSettings()
+            .setDoubleTapEnabled(false)
+            .setMaxZoom(10f)    ; // Falls back to max zoom level
     }
+    /*
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    */
 }
