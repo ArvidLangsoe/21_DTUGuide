e8699b858c9f0bb9bee3a9bc3c64a52260048219
Peter <peterxp@hotmail.fr>
2017-11-30 10:30:39 +0100

    Merge fix

--- app/src/main/java/com/arvid/dtuguide/data/DataTestActivity.java
+++ app/src/main/java/com/arvid/dtuguide/data/DataTestActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
